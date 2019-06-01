8d02697e463f2c0cac3b435eb33c080f13c2c1fc
Development user <dev@lennox>
2019-06-01 10:24:30 +0200

    vaukt and exustung etcd

---
+++ kubevault/cheatsheet.md
@@ -0,0 +1,9 @@
+# kubevault
+
+## post helm install
+```
+kubectl get pods --all-namespaces -l app=vault-operator --watch
+
+kubectl get crd -l app=vault
+
+```
---
+++ kubevault/setup.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+set -aeuo pipefail
+set -x 
+
+PROJECT=$1
+NAMESPACE=vault
+SERV_ACC=default
+f_usage(){
+echo "usage: $0"
+}
+
+
+cd ~/git/hub/vault-operator/example
+git pull
+sed -e "s/<namespace>/${NAMESPACE}/g"   -e "s/<service-account>/${SERV_ACC}/g"  rbac-template.yaml > rbac.yaml
+kubectl get ns $NAMESPACE > /dev/null ||kubectl create namespace $NAMESPACE
+kubectl apply -n $NAMESPACE  -f rbac.yaml
+## # etcd  operator from coreos
+# kubectl create -f etcd_crds.yaml
+# kubectl -n $NAMSPACE create -f etcd-operator-deploy.yaml
+cd -
+
+cd ~/git/y/ansible-playground/playbooks/
+ansible-playbook cluster-svc.yml -i inventory/cluster-full-init.yml --extra-vars="service=kubevault project=$PROJECT"
+cd -
+
+kubectl apply -f ~/git/y/vault/kubevault/VaultServer_existingetcd.yaml
+
+
+ 
+
